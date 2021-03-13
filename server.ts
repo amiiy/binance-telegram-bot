e6829a75d542d49d642b9d37a7c3991512f5ea5f
Amir Imani <this.amir.imani@gmail.com>
2021-03-13 19:00:47 +0400

    Set up telegram bot

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
-// @ts-expect-error `npm install express && npm install --save-dev @types/express`
+import * as dotenv from "dotenv"
 import express, { Request, Response } from 'express'
 import { Telegraf } from 'telegraf'
 
+dotenv.config()
 const token = process.env.BOT_TOKEN
 if (token === undefined) {
   throw new Error('BOT_TOKEN must be provided!')
@@ -10,15 +11,15 @@ if (token === undefined) {
 const bot = new Telegraf(token)
 // Set the bot response
 bot.on('text', (ctx) => ctx.replyWithHTML('<b>Hello</b>'))
-
+ 
 // Set telegram webhook
 // npm install -g localtunnel && lt --port 3000
-bot.telegram.setWebhook('https://----.localtunnel.me/secret-path')
+bot.telegram.setWebhook('https://terrible-starfish-51.loca.lt')
 
 const app = express()
 app.get('/', (req: Request, res: Response) => res.send('Hello World!'))
 // Set the bot API endpoint
-app.use(bot.webhookCallback('/secret-path'))
+app.use(bot.webhookCallback('/'))
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
 })
